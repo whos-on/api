7b934ecb4f19d2c768f82c398f8dd9c93c3c2fb9
Michael Ha <michaelhabao@gmail.com>
2023-04-24 09:04:06 -0400

    create/leave/sendMessage api endpoint

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,45 +1,154 @@
-const express = require('express');
-const User = require('../databases/schema/users');
-const Chat = require('../databases/schema/chats');
+const express = require("express");
+const User = require("../databases/schema/users");
+const Chat = require("../databases/schema/chats");
+const { request, response } = require("express");
 const router = express.Router();
 
 //Create a new chat between users
 //Incoming: users involved (array of username strings, where users[0] is the creator), a starting message
 //Outgoing: a chat is created and added to all the involved users' chat arrays, empty error/status 201
-router.post('/create', (request, response) => {
-    let users = [];
-    for(let i = 0; i < request.body.users.length; i++) {
-        users[i] = request.body.users[i];
-    }
-    const message = request.body.message;
-    const messageObj = {
-        sender: users[0],
-        contents: message,
-        timestamp: Date.now()
-    };
-    let checkedTimes = [];
-    for(let i = 0; i < users.length; i++) {
-        checkedTimes[i] = new Date(0);
-    }
-    checkedTimes[0] = Date.now();
-    const newChat = new Chat({
-        people: users,
-        messages: messageObj,
-        lastChecked: checkedTimes
-    });
-    newChat.save()
-    .then(async function() {
-        for(let i = 0; i < users.length; i++) {
-            await User.findOneAndUpdate({username: users[i]}, { $push: { chats: newChat._id } })
-            .catch((err) => {
-                response.status(404).send({error: err});
-            });
-        }
+router.post("/create", (request, response) => {
+  let users = [];
+  for (let i = 0; i < request.body.users.length; i++) {
+    users[i] = request.body.users[i];
+  }
+  const message = request.body.message;
+  const messageObj = {
+    sender: users[0],
+    contents: message,
+    timestamp: Date.now(),
+  };
+  let checkedTimes = [];
+  for (let i = 0; i < users.length; i++) {
+    checkedTimes[i] = new Date(0);
+  }
+  checkedTimes[0] = Date.now();
+  const newChat = new Chat({
+    people: users,
+    messages: messageObj,
+    lastChecked: checkedTimes,
+  });
+  newChat
+    .save()
+    .then(async function () {
+      for (let i = 0; i < users.length; i++) {
+        await User.findOneAndUpdate(
+          { username: users[i] },
+          { $push: { chats: newChat._id } }
+        ).catch((err) => {
+          response.status(404).send({ error: err });
+        });
+      }
     })
     .catch(function (err) {
-        console.log(err);
+      console.log(err);
     });
-    response.status(201).send({error: null});
+  response.status(201).send({ error: null });
 });
 
+// Send message between users in a same room
+// Incoming: id of the message group and who is the one to send the message
+// Outgoing: a new message has been sent
+router.post("/sendMessage", async (request, response) => {
+  const chatId = request.body.id;
+  const sender = request.body.sender;
+  const message = request.body.message;
+
+  // Check if chat room exists
+  const chatRoom = await Chat.findById(chatId);
+
+  if (!chatRoom) {
+    response.status(404).send({ error: "Chat room not found" });
+    return;
+  }
+
+  // Check if sender is a member of the chat room
+  if (!chatRoom.people.includes(sender)) {
+    response
+      .status(403)
+      .send({ error: "Sender is not a member of the chat room" });
+    return;
+  }
+
+  const userArray = chatRoom.people;
+  let index = 0;
+  for (let i = 0; i < chatRoom.people.length; i++) {
+    if (chatRoom.people[i] == sender) {
+      index = i;
+      break;
+    }
+  }
+  // Add message to existing chat room
+  const newMessage = {
+    sender: sender,
+    contents: message,
+    timestamp: Date.now(),
+  };
+
+  try {
+    await Chat.findByIdAndUpdate(
+      chatId,
+      {
+        $push: { messages: newMessage },
+        $set: { [`lastChecked.${index}`]: Date.now() },
+      },
+      { multi: true }
+    );
+  } catch (err) {
+    response.status(500).send({ error: err });
+    return;
+  }
+  response.status(200).send({ error: null });
+});
+
+// User want to leave a chat in the same room
+// Review chatId and userId
+
+router.post("/leave", async (request, response) => {
+  const chatId = request.body.chatId;
+  const userId = request.body.userId;
+
+  // Check if chat room exists
+  const chatRoom = await Chat.findById(chatId);
+
+  const userProfile = await User.findById(userId);
+  if (!userProfile) {
+    response.status(404).send({ error: "Can't find the user" });
+    return;
+  }
+
+  const username = userProfile.username;
+
+  if (!chatRoom) {
+    response.status(404).send({ error: "Chat room not found" });
+    return;
+  }
+  // Check if sender is a member of the chat room
+  if (!chatRoom.people.includes(username)) {
+    response
+      .status(403)
+      .send({ error: "User is not a member of the chat room" });
+    return;
+  }
+
+  try {
+    await User.updateOne({ _id: userId }, { $pull: { chats: `${chatId}` } });
+  } catch (err) {
+    response.status(500).send({ error: err });
+    return;
+  }
+
+  try {
+    await Chat.findByIdAndUpdate(chatId, {
+      $pull: { people: `${username}` },
+    });
+  } catch (err) {
+    response.status(500).send({ error: err });
+    return;
+  }
+
+  response.status(200).send({ error: null });
+});
+
+// ret.push(await Chat.findById(userObj.chats[i]))
 module.exports = router;
--- src/routes/friend.js
+++ src/routes/friend.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const User = require("../databases/schema/users");
 
@@ -6,80 +6,97 @@ const User = require("../databases/schema/users");
 //Incoming: user's object _id, username to search for
 //Outgoing: status 200/error will be empty
 router.put("/addFriend", async (request, response) => {
-    const {id, search} = request.body;
-    const userObj = await User.findById(id);
-    //search by username for the requestee, if not found, return an error
-    const friendReq = await User.findOne({username : search});
-    if(!friendReq) {
-        return response.status(404).send({error: "No user exists for that username!"});
-    }
-    await User.findByIdAndUpdate(friendReq._id, { $push: { requests: userObj.username } });
-    await User.findByIdAndUpdate(id, { $push: { pending: friendReq.username } });
-    return response.status(200).send({error: ""});
+  const { id, search } = request.body;
+  const userObj = await User.findById(id);
+  //search by username for the requestee, if not found, return an error
+  const friendReq = await User.findOne({ username: search });
+  if (!friendReq) {
+    return response
+      .status(404)
+      .send({ error: "No user exists for that username!" });
+  }
+  await User.findByIdAndUpdate(friendReq._id, {
+    $push: { requests: userObj.username },
+  });
+  await User.findByIdAndUpdate(id, { $push: { pending: friendReq.username } });
+  return response.status(200).send({ error: "" });
 });
 
 //Accept or reject a friend request, either way, remove the request from both sides
 //Incoming: user's object _id, username of the request sender, 1 to accept or 0 to reject
 //Outgoing: status 200/error will be empty
 router.put("/processRequest", async (request, response) => {
-    const id = request.body.id;
-    const requester = request.body.requester;
-    const accept = request.body.accept;
+  const id = request.body.id;
+  const requester = request.body.requester;
+  const accept = request.body.accept;
 
-    const userObj = await User.findById(id);
-    const newFriendObj = await User.findOne({username: requester});
-    const newFriend = newFriendObj._id;
-    //Check to see if the requester still exists
-    if(!newFriendObj) {
-        return response.status(404).send({error: "This user doesn't exist anymore!"});
-    }
-    //Remove the requester from your requests list, and remove the user from the requester's pending list
-    await User.findByIdAndUpdate(id, { $pull: { requests: requester } });
-    await User.findByIdAndUpdate(newFriend, { $pull: { pending: userObj.username } });
-    if(accept) {
-        //Add both users to their respective friend lists
-        await User.findByIdAndUpdate(id, { $push: { friends: newFriend } });
-        await User.findByIdAndUpdate(newFriend, { $push: { friends: id } });
-    }
-    return response.status(200).send({error: ""});
+  const userObj = await User.findById(id);
+  const newFriendObj = await User.findOne({ username: requester });
+  const newFriend = newFriendObj._id;
+  //Check to see if the requester still exists
+  if (!newFriendObj) {
+    return response
+      .status(404)
+      .send({ error: "This user doesn't exist anymore!" });
+  }
+  //Remove the requester from your requests list, and remove the user from the requester's pending list
+  await User.findByIdAndUpdate(id, { $pull: { requests: requester } });
+  await User.findByIdAndUpdate(newFriend, {
+    $pull: { pending: userObj.username },
+  });
+  if (accept) {
+    //Add both users to their respective friend lists
+    await User.findByIdAndUpdate(id, { $push: { friends: newFriend } });
+    await User.findByIdAndUpdate(newFriend, { $push: { friends: id } });
+  }
+  return response.status(200).send({ error: "" });
 });
 
 //Retrieve documents for all friends on a user's friend list
 //Incoming: user's object _id
 //Outgoing: array of friend documents called friends
 router.get("/get", async (request, response) => {
-    const id = request.body.id;
-    const userObj = await User.findById(id);
-    //our return array
-    let _ret = new Array();
-    for(let i = 0; i < userObj.friends.length; i++) {
-        _ret.push(await User.findById(userObj.friends[i]));
-    }
-    return response.status(200).send({friends: _ret});
+  const id = request.body.id;
+  const userObj = await User.findById(id);
+  //our return array
+  let _ret = new Array();
+
+  for (let i = 0; i < userObj.friends.length; i++) {
+    _ret.push(await User.findById(userObj.friends[i]));
+  }
+  return response.status(200).send({ friends: _ret });
 });
 
 //Find all friend documents that match a given query
 //Incoming: user's object _id, username/firstName/lastName to search for
 //Outgoing: array of matching friend documents called friends
 router.get("/search", async (request, response) => {
-    const {id, search} = request.body;
-    userObj = await User.findById(id);
-    let _ret = new Array();
-    const regex = new RegExp("^" + search + "[\w]*", "i");
-    for(let i = 0; i < userObj.friends.length; i++) {
-        //Flag for when our search comes up empty for this id
-        let notFound = 0;
-        //We $and with the id so that we only get users who are on the user's friend list
-        let result = await User.findOne( {$and: [{_id: userObj.friends[i]}, { $or: [{ username: {$regex: regex} }, { firstName : {$regex: regex} }, { lastName : {$regex: regex}}] }] } )
-        .catch(err => {
-            notFound = 1;
-        });
-        if(result == undefined | result == null)
-            continue;
-        if(!notFound)
-            _ret.push(result);
-    }
-    return response.status(200).send({friends: _ret});
+  const { id, search } = request.body;
+  userObj = await User.findById(id);
+  let _ret = new Array();
+  const regex = new RegExp("^" + search + "[w]*", "i");
+  for (let i = 0; i < userObj.friends.length; i++) {
+    //Flag for when our search comes up empty for this id
+    let notFound = 0;
+    //We $and with the id so that we only get users who are on the user's friend list
+    let result = await User.findOne({
+      $and: [
+        { _id: userObj.friends[i] },
+        {
+          $or: [
+            { username: { $regex: regex } },
+            { firstName: { $regex: regex } },
+            { lastName: { $regex: regex } },
+          ],
+        },
+      ],
+    }).catch((err) => {
+      notFound = 1;
+    });
+    if ((result == undefined) | (result == null)) continue;
+    if (!notFound) _ret.push(result);
+  }
+  return response.status(200).send({ friends: _ret });
 });
 
 module.exports = router;
