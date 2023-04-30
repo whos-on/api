5cc27b8ac098b7d21d84d8d1ebfe262c56179f15
Skyclo <hello.skyclo@gmail.com>
2023-04-29 23:11:21 -0400

    ⚡️ switch to in-memory mongo server to improve test performance

--- tests/jestpreflight.js
+++ tests/jestpreflight.js
@@ -1,20 +1,26 @@
 const mongoose = require("mongoose")
+const { MongoMemoryServer } = require("mongodb-memory-server")
 
 module.exports = () => {
+    let mongoServer, mongoUri
+
     if (process.env.NODE_ENV == "production") {
         console.log("Cannot run tests in production mode")
         process.exit(1)
     }
 
     beforeEach(async () => {
-        return await mongoose.connect(process.env.MONGODB_URL, {
+        mongoServer = await MongoMemoryServer.create()
+        await mongoose.connect(mongoUri = mongoServer.getUri(), {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
+        return await mongoose.connection.db.dropDatabase()
     })
 
     afterEach(async () => {
-        await mongoose.connection.db.dropCollection("users")
-        return await mongoose.connection.close()
+        await mongoose.connection.db.dropDatabase()
+        await mongoose.connection.close()
+        return await mongoServer.stop()
     })
 }
